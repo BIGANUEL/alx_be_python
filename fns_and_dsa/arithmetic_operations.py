737dfad15128e1c03ace041fbac5830f8cb04ee1
Amanuel Wubishet <biganuel6792@gmail.com>
2024-09-28 20:15:03 +0300

    arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation (num1, num2, operation) :
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -7,8 +7,8 @@ def perform_operation (num1, num2, operation) :
         return num1 * num2
     elif operation == 'divide':
         if num2 != 0:
-             return num1 / num2
+            return num1 / num2
         else:
             return "Error: Division by zero"
     else:
-            return "Invalid operation"
+        return "Invalid operation"
