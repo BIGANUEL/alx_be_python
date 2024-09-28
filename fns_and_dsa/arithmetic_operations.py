c60337073fcc6c3df9db12358abde501601eb146
BIGANUEL <162797908+BIGANUEL@users.noreply.github.com>
2024-09-28 20:17:08 +0300

    arithmetic_operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,9 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
+        if num2 == 0:
+             return "Error: Division by zero"
         else:
-            return "Error: Division by zero"
+            return num1 / num2
     else:
         return "Invalid operation"
