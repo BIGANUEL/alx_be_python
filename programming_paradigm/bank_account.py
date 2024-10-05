f602ef5cf4d7af43561270aa4d0c94952f709f06
Amanuel Wubishet <biganuel6792@gmail.com>
2024-10-05 22:48:18 +0300

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -11,6 +11,6 @@ class BankAccount:
         else:
             return False
     def display_balance(self):
-        print (f"Current Balance: ${self.account_balance}")
+        print (f"Current Balance: ${self.account_balance:.2f}")
     
     
