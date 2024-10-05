f5d6bddf0a423b5034a08c30cade2092c8b37588
Amanuel Wubishet <biganuel6792@gmail.com>
2024-10-05 22:43:05 +0300

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self,account_balance ):
+    def __init__(self,account_balance = 0 ):
         self.account_balance = account_balance
     
     def deposit(self,amount):
