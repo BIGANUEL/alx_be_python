ea1812a0e6f42cdac0a7d82bc7e4e6ff30260087
Amanuel Wubishet <biganuel6792@gmail.com>
2024-10-06 15:12:34 +0300

    write unit test

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,20 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+    def test_add(self):
+        self.assertEqual(self.calc.add(3,5), 8)
+        self.assertEqual(self.calc.add(4,6), 10)
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(3,5),-2)
+        self.assertEqual(self.calc.subtract(9,9),0)
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3,5),15)
+        self.assertEqual(self.calc.multiply(9,9),81)
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(15,3),5)
+        self.assertEqual(self.calc.divide(9,9),1)
+        self.assertEqual(self.calc.divide(9,0),None)
